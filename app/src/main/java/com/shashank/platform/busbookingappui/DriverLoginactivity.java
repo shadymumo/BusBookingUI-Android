698192c0cda52c9f71bfda828e361eede29885e5
ShadyMumo <shadymumo8@gmail.com>
2023-01-19 11:22:50 +0300

    style theme

--- app/src/main/java/com/shashank/platform/busbookingappui/DriverLoginactivity.java
+++ app/src/main/java/com/shashank/platform/busbookingappui/DriverLoginactivity.java
@@ -1,14 +1,13 @@
 package com.shashank.platform.busbookingappui;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class WelcomePageActivity extends AppCompatActivity {
+public class DriverLoginactivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_page);
+        setContentView(R.layout.activity_driver_loginactivity);
     }
 }
